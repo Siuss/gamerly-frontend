327fb56074c819f1baa38fb8a48c4c24612e33db
berardibraian23 <berardibraian9423@hotmail.com>
2024-04-26 19:54:17 -0300

    nuevo cambio de App

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import FotoDePerfil from './src/componentes/atomos/fotoDePerfil/FotoDePerfil';
-import Parrafo from './src/componentes/atomos/parrafo/Parrafo';
+import { View } from 'react-native';
 import HeaderTitle from './src/componentes/header/headerTitle';
 import { BusquedaDeJugadores } from "./src/componentes/vistas/BusquedaDeJugadores";
 
 export default function App() {
   const titulo = 'Home';
   return (
-    <View style={styles.container}>
+    <View>
       <HeaderTitle title={titulo} />
-      <FotoDePerfil src='https://www.civitatis.com/f/argentina/bariloche/free-tour-bariloche-589x392.jpg'/>
-      <Parrafo variante='blancoS' subrayado>hola confundido</Parrafo>
-      <BusquedaDeJugadores />;
-      <StatusBar style="auto" />
+      <BusquedaDeJugadores />
     </View>
   )}
