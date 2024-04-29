445e63622422460446ad6c23daccdecd8907829c
berardibraian23 <berardibraian9423@hotmail.com>
2024-04-29 05:53:58 -0300

    Agregado de rutas a la AppRutas

--- src/componentes/header/headerTitle.jsx
+++ src/componentes/header/headerTitle.jsx
@@ -3,45 +3,37 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { Color } from '../../estilos/colores'
 import { Ionicons } from '@expo/vector-icons'
 import { Gradiente } from "../atomos/gradiente/Gradiente"
-/* import { useNavigation } from '@react-navigation/native' */
+import { useNavigation } from '@react-navigation/native'
 
-export const HeaderTitle = ({ title, onBackPress }) => {
-  // const navigation = useNavigation();
+export const HeaderTitle = ({ title }) => {
+  const navigation = useNavigation()
 
-  // const handleNavigate = () => {
-  //   navigation.goBack();
-  // };
-
-  return (
-      <Gradiente variante="gradienteHorizontal" style={styles.header}>
-      <TouchableOpacity onPress={onBackPress} style={styles.button}>
-        <Ionicons name="arrow-back" size={24} color={Color.blanco} />
-      </TouchableOpacity>
-      <Text style={styles.title}>{title}</Text>
-      <View style={{ flex: 1 }} />
-      </Gradiente>   
-  )
-}
-
-const cambiarTitulo = () => {
-  const titulos = {
-    '/home': 'Home',
-    '/perfil': 'Mi Perfil',
-    '/reseñas': 'Reseñas',
-    '/busquedaAvanzada': 'Búsqueda Avanzada',
+  const handleNavigateBack = () => {
+    navigation.goBack();
   };
 
-  const matchingRoute = Object.keys(titulos).find((route) => {
-    const regex = new RegExp(`^${route}$`)
-  });
+  /* const handleNavigate = () => {
+    navigation.navigate('resenias');
+  }; */
 
-  return titulos[matchingRoute] || 'Home'
+  return (
+    <Gradiente variante="gradienteHorizontal" style={styles.header}>
+      <TouchableOpacity onPress={handleNavigateBack} style={styles.button}>
+        <Ionicons name="arrow-back" size={24} color={Color.blanco} />
+      </TouchableOpacity>
+      {/* <TouchableOpacity onPress={handleNavigate} style={styles.button}>
+        <Ionicons name="arrow-back" size={24} color={Color.blanco} />
+      </TouchableOpacity> */}
+      <Text style={styles.title}>{title}</Text>
+      <View style={{ flex: 1 }} />
+    </Gradiente>   
+  );
 };
-const titulo = cambiarTitulo()
+
+
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: Color.primario,
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: Color.primario,
@@ -53,14 +45,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'white',
     textAlign: 'center',
-  },
-  buttonText: {
-    fontSize: 20,
-    color: 'white',
+    flexBasis: "80%"
   },
   button: {
     marginRight: 16,
   },
-});
+})
 
 export default HeaderTitle
