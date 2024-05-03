d3ed20311d19342ee6084eee6e6f8a6bd9ddb0b7
veronicacarbonelli <veronicacarbonelli60@gmail.com>
2024-05-03 01:02:27 -0300

    me traje los cambios de las ramas 22, 29 y 25 para crear perfil

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native-web'; // Importa componentes de react-native-web
+import FotoDePerfil from './src/componentes/atomos/fotoDePerfil/FotoDePerfil';
+import { BusquedaDeJugadores } from "./src/componentes/vistas/BusquedaDeJugadores";
+import Parrafo from './src/componentes/atomos/parrafo/Parrafo';
+import HeaderTitle from './src/componentes/header/headerTitle';
 import NavBar from "./src/componentes/bloques/Navbar";
-import {BusquedaDeJugadores} from"./src/componentes/vistas/BusquedaDeJugadores";
-import { View } from "react-native-web";
 
 export default function App() {
+  const titulo = 'Home';
   return (
-  <View>
-    <BusquedaDeJugadores/>
-    <NavBar/>
-
-  </View>
-
-  
+    <View style={styles.container}>
+      <HeaderTitle title={titulo} />
+      {/* <FotoDePerfil src='https://www.civitatis.com/f/argentina/bariloche/free-tour-bariloche-589x392.jpg' />
+      <BusquedaDeJugadores />*/}
+ 
+      <StatusBar style="auto" /> 
+     
+      <NavBar />
+    </View>
   );
-  
-
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/componentes/header/headerTitle.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Color } from '../../estilos/colores'
+import { Ionicons } from '@expo/vector-icons'
+// import { useNavigation } from '@react-navigation/native'
+
+export const HeaderTitle = ({ title, onBackPress }) => {
+  // const navigation = useNavigation();
+
+  // const handleNavigate = () => {
+  //   navigation.goBack();
+  // };
+
+  return (
+    /* <Gradiente variante="gradienteHorizontal" style={styles.card}> */
+    <View style={styles.header}>
+      <TouchableOpacity onPress={onBackPress} style={styles.button}>
+        <Ionicons name="arrow-back" size={24} color={Color.blanco} />
+      </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
+      <View style={{ flex: 1 }} />
+    </View>
+    /* </Gradiente> */
+  )
+}
+
+const cambiarTitulo = () => {
+  const titulos = {
+    '/home': 'Home',
+    '/perfil': 'Mi Perfil',
+    '/reseñas': 'Reseñas',
+    '/busquedaAvanzada': 'Búsqueda Avanzada',
+  };
+
+  const matchingRoute = Object.keys(titulos).find((route) => {
+    const regex = new RegExp(`^${route}$`)
+  });
+
+  return titulos[matchingRoute] || 'Home'
+};
+const titulo = cambiarTitulo()
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: Color.primario,
+    height: 60,
+    paddingHorizontal: 20,
+  },
+  title: {
+    flex: 1,
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  button: {
+    marginRight: 16,
+  },
+});
+
+export default HeaderTitle
