622343c5d653024fadc6d3fdc34476f057294700
salamida <salamida@fktech.net>
2024-05-07 00:06:27 -0300

    Se completan divisores y descripciones de búsqueda avanzada.

--- src/componentes/vistas/BusquedaAvanzada.jsx
+++ src/componentes/vistas/BusquedaAvanzada.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Title } from "react-native";
+
+import { Parrafo } from "../atomos/parrafo/Parrafo";
+import BarraBusqueda from "../atomos/barraBusqueda/BarraBusqueda";
+import { Divisor } from "../atomos/divisor/Divisor";
+
+import { ListaDePildoras } from "../bloques/ListaDePildoras";
 
 import { Color } from "../../estilos/colores";
-import { Divisor } from "../atomos/divisor/Divisor";
-import BarraBusqueda from "../atomos/barraBusqueda/BarraBusqueda";
-import { ListaDePildoras } from "../bloques/ListaDePildoras";
 
 const juegos = [
   {
@@ -23,6 +26,30 @@ const juegos = [
     id: 4,
     name: "World of Warcraft",
   },
+  {
+    id: 5,
+    name: "Valorant"
+  },
+  {
+    id: 6,
+    name: "Rocket League"
+  },
+  {
+    id: 7,
+    name: "Fortnite"
+  },
+  {
+    id: 8,
+    name: "Apex Legends"
+  },
+  {
+    id: 9,
+    name: "PUBG"
+  },
+  {
+    id: 10,
+    name: "Hearthstone"
+  },
 ];
 
 export const BusquedaAvanzada = () => {
@@ -30,10 +57,20 @@ export const BusquedaAvanzada = () => {
   
   return (
     <View style={styles.container}>
-      <Text>Juevos en común</Text>
+      <Parrafo variante="blancoM">Juevos en común</Parrafo>
       <BarraBusqueda text={searchText} onChangeText={onChangeSearchText}/>
       <ListaDePildoras items={juegos}/>
+      <Parrafo variante="blancoM">Reseña</Parrafo>
+      {/* TODO: Barra de RESEÑA */}
+      <BarraBusqueda text={searchText} onChangeText={onChangeSearchText}/>
       <Divisor />
+      <Parrafo variante="blancoM">Disponibilidad horaria</Parrafo>
+      <Parrafo variante="blancoM">Días de la semana</Parrafo>
+      {/* TODO: Selector de DIAS */}
+      <Parrafo variante="blancoM">Horario</Parrafo>
+      {/* TODO: Selector de HORARIOS */}
+      <Divisor />
+      {/* TODO: Botonera */}
     </View>
   );
 };
@@ -42,6 +79,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Color.neutro,
     width: "100%",
-    height: "100%"
+    height: "100%",
+
+    
   },
 });
