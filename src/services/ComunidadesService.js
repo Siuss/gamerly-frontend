3e5e1b27537e9e86a9417653c330ace16c35dca9
salamida <salamida@fktech.net>
2024-06-18 00:43:15 -0300

    Se intalan depencias usadas en la rama, y se plantea el endpoint 'Comunidades'.

---
+++ src/services/ComunidadesService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { BASE_URL } from './requestConfig';
+
+// Función para obtener las comunidades desde el endpoint
+const obtenerComunidades = async () => {
+  try {
+    // Realiza la petición GET usando axios
+    const response = await axios.get(
+        `${BASE_URL}${SesionEndpoints.getAllComunidad}`
+    );
+    
+    // Si la respuesta es exitosa, devuelve los datos
+    if (response.status === 200) {
+      console.log('Datos de comunidades:', response.data);
+      return response.data;
+    }
+  } catch (error) {
+    // Maneja cualquier error que ocurra durante la petición
+    console.error('Error al obtener las comunidades:', error);
+  }
+};
+
+// Llama a la función para obtener las comunidades
+obtenerComunidades();
