ddaf5810d000a6d8fb6f6d454e9e6135b5532bca
veronicacarbonelli <veronicacarbonelli60@gmail.com>
2024-06-22 01:52:53 -0300

    Se crea la pagina de dejar resenia o comentario

---
+++ src/componentes/atomos/TextArea/TextArea.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import { Color } from "../../../estilos/colores";
+
+export default function TextArea(props) {
+  const {style, ...restProps} = props;
+
+  return (
+    <TextInput
+      style={[styles.input, style]}
+      value={props.text}
+      onChangeText={props.onChangeText}
+      placeholder="Deja tu mensaje"
+      multiline={true}
+      {...restProps}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+     width: "100%",
+    height: 150,
+    padding: 16,
+    color: Color.secundario,
+    fontSize: 16,
+    borderRadius:40,
+    border:`1px solid ${Color.secundario}`,
+  },
+});
