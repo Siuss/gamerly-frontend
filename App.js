825cd2f6d117fa39d4eb2ae31284afd9be0617cd
stephanie <stephaniepazos89@gmail.com>
2024-07-17 17:01:36 -0300

    deshabilitar warnings innecesarios

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AppRutas from "./src/componentes/rutas/AppRutas";
-import { StyleSheet } from "react-native";
+import { LogBox, StyleSheet } from "react-native";
 import { Color } from "./src/estilos/colores";
 import { ToastProvider } from "./src/providers/toast-provider/ToastProvider";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App = () => {
+  LogBox.ignoreAllLogs()
+
   return (
     <SafeAreaProvider>
       <ToastProvider>
@@ -14,14 +16,5 @@ const App = () => {
     </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  toast: {
-    backgroundColor: Color.primario,
-  },
-  toastText: {
-    color: Color.blanco,
-  },
-});
 
 export default App;
